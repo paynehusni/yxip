7521a8045c44ace790ec7815cf0a452eb28dc6ef
paynehusni <42644295+paynehusni@users.noreply.github.com>
2024-04-22 00:36:21 +0800

    update

--- merge.py
+++ merge.py
@@ -8,8 +8,23 @@ urls = [
 merged_content = ""
 
 for url in urls:
-    response = requests.get(url)
-    merged_content += response.text + "\n"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # 如果响应状态码不是200,则抛出异常
+        lines = response.text.strip().split("\n")
+        
+        if "bestcf" in url:
+            suffix = ":8433#bestcf"
+        elif "bestproxy" in url:
+            suffix = ":8433#bestproxy"
+        else:
+            suffix = ""
+        
+        for line in lines:
+            merged_content += line + suffix + "\n"
+    
+    except requests.exceptions.RequestException as e:
+        print(f"请求 {url} 时出错: {e}")
 
 with open("yxip.txt", "w") as file:
     file.write(merged_content)
